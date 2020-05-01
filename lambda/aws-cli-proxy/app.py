7e4d1dffd8b21221da8ba9897d758f6b20a7af9e
Mat Werber <matwerber@gmail.com>
2020-05-01 11:13:35 -0700

    update

--- lambda/aws-cli-proxy/app.py
+++ lambda/aws-cli-proxy/app.py
@@ -22,15 +22,22 @@ def run_command(command):
         logger.error(response)
     return response
 
+# At the moment, the Lambda will execute the passed command with whatever role
+# is attached to the Lambda function. The Cognito app is passiing the user's
+# identity pool temp STS credentials, I just don't know how to assume them
+# for the CLI. If we were doing everything in boto3, that would be easy...
+
 def lambda_handler(event, context):
 
-    #
-    # event: {
-    #   commandToRun: "aws s3api get-bucket-acl --bucket werberm-example-bucket --output json"
-    # }
-    #
-
     print("Received event: \n" + json.dumps(event, indent=2))
+
+    access_key = event['credentials']['accessKeyId']
+    secret_key = event['credentials']['secretAccessKey']
+    session_token = event['credentials']['sessionToken']
+    
+    #run_command('export AWS_ACCESS_KEY_ID={}'.format(access_key))
+    #run_command('export AWS_SECRET_ACCESS_KEY_ID={}'.format(secret_key))
+    #run_command('export AWS_SESSION_TOKEN={}'.format(session_token))
 
     commandToRun = event['commandToRun']
     commandArray = commandToRun.split(" ", 1)
@@ -39,7 +46,8 @@ def lambda_handler(event, context):
 
     if (commandArray[0] == 'aws'):
         commandResult = run_command('/opt/aws {}'.format(commandArray[1]))
-        
+        #final_command = '(AWS_ACCESS_KEY_ID={} && AWS_SECRET_ACCESS_KEY_ID={} && AWS_SESSION_TOKEN={} && /opt/aws {})'.format(access_key,secret_key,session_token,commandArray[1])
+        #commandResult = run_command(final_command)
     else:
         commandResult = "Exception: not a valid AWS CLI command."
         logger.error(commandResult)
